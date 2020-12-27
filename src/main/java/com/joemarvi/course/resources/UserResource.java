10303325ee828f4f82729da72590854da4504b7b
joelson-dev-joe <joelson.dh@gmail.com>
2020-12-26 22:27:27 -0300

    Exception handling - findById

--- src/main/java/com/joemarvi/course/resources/UserResource.java
+++ src/main/java/com/joemarvi/course/resources/UserResource.java
@@ -3,7 +3,6 @@ package com.joemarvi.course.resources;
 import java.net.URI;
 import java.util.List;
 
-import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
